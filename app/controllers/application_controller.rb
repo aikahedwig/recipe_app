d819759adb7a209bfa8eacdc1705b9d7118be911
aikahed <aikahedwig687@gmail.com>
2024-06-03 13:35:20 +0900

    update ERD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,46 +1,13 @@
-# class ApplicationController < ActionController::Base
-#     def styles
-#         send_file "#{Rails.root}/app/assets/stylesheets/styles.css", type: 'text/css', disposition: 'inline'
-#     end
-#     def require_admin
-#       unless current_user&.admin?
-#         redirect_to root_path, alert: 'アドミン権限がありません'
-#     end
-#   end
-# end
 class ApplicationController < ActionController::Base
-  # Deviseなどの認証ソリューションを使用する場合は、
-  # その設定を行うコードを記述する
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:new, :create]
 
-  # ログインユーザーの取得メソッド
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  # Deviseの設定を追加
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  helper_method :current_user
-
-  # ユーザーがログインしているかどうかを判別するメソッド
-  def logged_in?
-    !!current_user
-  end
-  helper_method :logged_in?
-
-  # アクションの前に実行する処理を定義する(例: 認証チェック)
-  before_action :authenticate_user!
-
-  # アドミン権限のチェックメソッド
-  def require_admin
-    unless current_user&.admin?
-      redirect_to root_path, alert: 'アドミン権限がありません'
-    end
-  end
-
-  # その他、アプリケーション全体で使用する補助メソッドを定義する
-
-  private
-
-  # 認証関連の処理を記述する(例: devise_parameter_sanitizer)
-
 end
-    
-    
-      
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,15 @@
 class SessionsController < ApplicationController
+  before_action :authenticate_user!, only: [:destroy]
+  
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
-    end
+    # Deviseが自動的に認証処理を行う
   end
 
   def destroy
-    log_out 
-    redirect_to root_url
+    sign_out current_user
+    redirect_to root_path
   end
 end
